3adb1d944e05b7c9b27f5df1d352487c46e4ecfb
Jakub Lapinski <jakub.lapinski22@imperial.ac.uk>
2025-01-27 22:04:53 +0000

    Przydupasy są funkcjonalne

--- Madaka/Characters/player.gd
+++ Madaka/Characters/player.gd
@@ -2,7 +2,11 @@ class_name Player extends CharacterBody2D
 
 
 var SPEED = 100.0
+var slow_modifier = 0
 @onready var health : Health = $Health
+@onready var timer = $Timer
+
+signal slowed(amount: float, knockback: Vector2)
 
 
 func _physics_process(delta):
@@ -11,9 +15,20 @@ func _physics_process(delta):
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	
-	velocity = SPEED * direction;
+	
+	
+	velocity = (SPEED * (1 - slow_modifier)) * direction;
 	move_and_slide()
 
 
 func _on_health_damaged(amount, knockback):
 	print("player damaged by: %d" % amount)
+	
+func apply_slow(amount, duration):
+	timer.start(duration)
+	slow_modifier = amount
+	print("Timer sta")
+
+func _on_timer_timeout():
+	slow_modifier = 0
+	print("Timer stop")
